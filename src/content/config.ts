cbf240428b2e1dce67a1b27a1709e2741c95b24a
Destrifer <destrifer@yandex.ru>
2024-10-31 12:20:58 +0300

    first

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// apiFood.js
+import { defineCollection } from "astro:content";
+
+const apiFood = defineCollection({
+	loader: async () => {
+		const response = await fetch("https://new.mycarrental.ru/api/v2/search_cars?from-id=10&to-id=10&to-date=10.11.2024&from-date=04.11.2024&from-time=12%3A00&to-time=12%3A00&promocode=null&per_page=100");
+		const data = await response.json();
+		const vehicles = data.vehicles;
+		return vehicles.map((vehicle: any) => ({
+			id: vehicle.id,
+			...vehicle,
+		}));
+	},
+});
+
+export const collections = {
+	apiFood,
+};
