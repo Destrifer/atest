7bb36486fbc4a3c44e786cbe807dbb56000ed9c8
Destrifer <destrifer@yandex.ru>
2024-10-31 12:27:48 +0300

    try deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	experimental: { contentLayer: true }
+  output: 'server',
+  experimental: { contentLayer: true },
+  adapter: netlify()
 });
