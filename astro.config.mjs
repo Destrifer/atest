3d667e0777e9a17176efcb23ab2f77372d6d8a0c
Destrifer <destrifer@yandex.ru>
2024-10-31 13:04:22 +0300

    add content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import tailwind from '@astrojs/tailwind';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   experimental: { contentLayer: true },
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [tailwind(), icon()]
 });
