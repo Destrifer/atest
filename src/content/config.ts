b4e7db4cb91b02f46da6f5506181cf20f6988d42
Destrifer <destrifer@yandex.ru>
2024-11-01 16:58:52 +0300

    fix config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,27 @@
 import { defineCollection } from "astro:content";
 
+// Функция для вычисления даты с добавлением дней и форматированием в "DD.MM.YYYY"
+function getDateWithOffset(daysOffset) {
+	const date = new Date();
+	date.setDate(date.getDate() + daysOffset);
+	const day = String(date.getDate()).padStart(2, '0');
+	const month = String(date.getMonth() + 1).padStart(2, '0'); // Месяцы от 0 до 11
+	const year = date.getFullYear();
+	return `${day}.${month}.${year}`; // Формат "DD.MM.YYYY"
+}
+
 const apiCars = defineCollection({
 	loader: async () => {
-		const response = await fetch("https://new.mycarrental.ru/api/v2/search_cars?from-id=10&to-id=10&to-date=10.11.2024&from-date=04.11.2024&from-time=12%3A00&to-time=12%3A00&promocode=null&per_page=100");
+		// Устанавливаем значения по умолчанию для дат
+		const fromDate = getDateWithOffset(3);
+		const toDate = getDateWithOffset(10);
+
+		const response = await fetch(
+			`https://new.mycarrental.ru/api/v2/search_cars?from-id=10&to-id=10&to-date=${toDate}&from-date=${fromDate}&from-time=12%3A00&to-time=12%3A00&promocode=null&per_page=100`
+		);
 		const data = await response.json();
 		const vehicles = data.vehicles;
-		return vehicles.map((vehicle: any) => ({
+		return vehicles.map((vehicle) => ({
 			id: vehicle.id,
 			...vehicle,
 		}));
