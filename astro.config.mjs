4270e3be8daada41a74fc1e873d2be5090453bff
Destrifer <destrifer@yandex.ru>
2024-11-06 20:55:25 +0300

    add backgr

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import { astroImageTools } from "astro-imagetools";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  experimental: { contentLayer: true },
-  adapter: netlify(),
-  integrations: [tailwind(), icon()]
+	output: 'server',
+	experimental: { contentLayer: true },
+	adapter: netlify(),
+	integrations: [tailwind(), icon(), astroImageTools]
 });
